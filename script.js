a899bd85cccc558675f631256af60747be02880f
MoonisTran <96872572+MoonisTran@users.noreply.github.com>
2022-01-15 16:12:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const btnAdd = document.getElementById("add-btn");
-// btnAdd.onclick = add();
-const btnRemoveAll = document.getElementById("remove-btn")
-const list = document.getElementById("list-wrapper")
+const btnRemoveAll = document.getElementById("remove-btn");
+const list = document.getElementById("listTodo");
 
-const new_status_color_code = "cyan";
-const done_status_color_code = "green";
-const processing_status_color_code = "red";
-
-
-//Nút Đổi trạng thái => Mới => đang làm, đang làm => mới
-
-//Nút hoàn tất , Mới || đang làm => Done, Done => Mới.
+const new_status_color_code = "green";
+const done_status_color_code = "darkblue";
+const processing_status_color_code = "orange";
 
 let counter = 0;
 
 function add() {
-    counter++;
-    const div = document.createElement("li")
-
-    const itemLabel = document.createElement("span")
-    itemLabel.innerHTML = "Task " + counter;
+    let name = prompt ("Nhập tên subject")
+    let deadline = prompt ("Deadline date")
+    let nguoiLam = prompt ("Người thực hiện")
+    const div = document.createElement("li");
+    const itemLabel = document.createElement("span");
+    itemLabel.innerHTML = `Sub: ${name} | Deadline: ${deadline} | Người làm: ${nguoiLam}`
     itemLabel.style.color = new_status_color_code;
     div.dataset.status = "new";
-    const doneBtn = document.createElement("button")
+    const doneBtn = document.createElement("button");
     doneBtn.innerHTML = "Done";
-
-    const changeStatusBtn = document.createElement("button")
+    const changeStatusBtn = document.createElement("button");
     changeStatusBtn.innerHTML = "Status";
-
+    const removetd = document.createElement("button");
+    removetd.innerHTML = "Delete";
+    const detail = document.createElement("button");
+    detail.innerHTML = "Details";
+    
     div.appendChild(itemLabel)
+    div.appendChild(detail)
     div.appendChild(changeStatusBtn)
     div.appendChild(doneBtn)
+    div.appendChild(removetd)
+
     doneBtn.onclick = function () {
-        // if (["new","process"].includes(this.parentNode.dataset.status)) { Tham khảo
         if (this.parentNode.dataset.status === "new" || this.parentNode.dataset.status === "process") {
             itemLabel.style.color = done_status_color_code;
             this.parentNode.dataset.status = "done";
@@ -51,10 +51,19 @@ function add() {
         }
     }
 
+    detail.onclick = function(){
+        counter++;
+        let putInDetail = prompt ("Nhập chi tiết:")
+        const paraDetail = document.createElement("p");
+        paraDetail.innerHTML = `Details ${counter}: ${putInDetail}`
+        div.appendChild(paraDetail)
+    }   
     list.appendChild(div)
-}
-
-function removeAll() {
-    list.innerHTML = "";
+    removetd.onclick = function(){
+        list.removeChild(div);
+    }
     counter = 0;
+}
+function remove() {
+    list.innerHTML = "";
 }
